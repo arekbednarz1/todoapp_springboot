1994a0890d89a9c36319f27e0ebf64ffe1578e24
arekbednarz1 <arkadiusz_bednarz@o2.pl>
2021-02-15 20:05:44 +0100

    poprawa kody controller

--- src/main/java/pl/arekbednarz/todoapp/controller/TaskController.java
+++ src/main/java/pl/arekbednarz/todoapp/controller/TaskController.java
@@ -1,14 +1,11 @@
 package pl.arekbednarz.todoapp.controller;
 
 
-import jdk.jfr.ContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import pl.arekbednarz.todoapp.model.Task;
@@ -18,63 +15,58 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-//@RepositoryRestController
+
 @RestController
-//@Controller
-//@RequestMapping(path = "/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
-
     private final TaskRepository repository;
 
     @Autowired
-//    Autowired nie trzeba do wstrzykiwania
      TaskController(TaskRepository repository) {
         this.repository = repository;
     }
 
-    @PostMapping("/tasks")
-//    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate){
         Task result = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/"+result.getId())).body(result);
     }
 
 
-//    @RequestMapping(method = RequestMethod.GET, path ="/tasks")
-//    albo tak
-//    params znaczy ze wchodzimy w ten kontroler tylko wtedy gdy nie ma tych parametrow
-
-        @GetMapping(value = "/tasks", params = {"!sort","!page","!size"})
-//    @RequestMapping(method = RequestMethod.GET, params = {"!sort","!page","!size"})
+    @GetMapping(params = {"!sort","!page","!size"})
     ResponseEntity<List<Task>> readAllTasks(){
-//    zamiennie z tym
-//    List<Task> readAllTasks(){
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
-//        return repository.findAll();
     }
 
 //    pageable to obiekt parametr stronicowania
-    @GetMapping("/tasks")
-//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
-    @GetMapping("/tasks/{id}")
+    @GetMapping("/{id}")
 //    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     ResponseEntity<Task>readTask(@PathVariable long id){
         return repository.findById(id)
                 .map(task -> ResponseEntity.ok(task))
                 .orElse(ResponseEntity.notFound().build());
     }
+    @GetMapping("/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state){
+        return ResponseEntity.ok(
+                repository.findByDone(state)
+        );
+
+
+    }
 
 
 
-    @PutMapping("/tasks/{id}")
+    @PutMapping("/{id}")
 //    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
     ResponseEntity<?> updateTask(@PathVariable long id, @RequestBody @Valid Task toUpdate){
         if (!repository.existsById(id)){
@@ -92,7 +84,7 @@ public class TaskController {
 
 //    transakcja musi byc na publicznej metodzie. ta metoda zmienia boolean true false czyli aktualizuje
     @Transactional
-    @PatchMapping("/tasks/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> toggleTask(@PathVariable long id){
         if (!repository.existsById(id)){
             ResponseEntity.notFound().build();
