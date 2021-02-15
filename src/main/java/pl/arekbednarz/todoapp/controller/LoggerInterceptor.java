a27b65d91195dcb94b0eb5991259e16c1a14eeec
arekbednarz1 <arkadiusz_bednarz@o2.pl>
2021-02-15 21:29:46 +0100

    kierunek dzialania

--- src/main/java/pl/arekbednarz/todoapp/controller/LoggerInterceptor.java
+++ src/main/java/pl/arekbednarz/todoapp/controller/LoggerInterceptor.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,12 @@ class LoggerInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("[doFilter] "+ request.getMethod()+ " "+request.getRequestURI());
+        logger.info("[preHandle] "+ request.getMethod()+ " "+request.getRequestURI());
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("[postHandle]");
     }
 }
